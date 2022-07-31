20a7bb99304bbb02e7257176b93cf352f4456502
hzm <934585316@qq.com>
2022-07-31 21:07:59 +0800

    fix: deploy config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,8 +24,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh'],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
